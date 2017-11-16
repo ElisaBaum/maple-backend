f4c1d764d04445ae5daef299a7fe273face2c174
Elisa Baum <elisabaum@hotmail.com>
2017-11-16 21:09:49 +0100

    added dynamic content controller, model and migration
    * clean up db migrations
    * use auth middleware globally

---
+++ lib/dynamic-content/DynamicContentController.ts
@@ -0,0 +1,17 @@
+import {Get, JsonController, Param} from "routing-controllers";
+import {DynamicContent} from "./models/DynamicContent";
+import {NotFoundError} from "../common/NotFoundError";
+
+@JsonController()
+export class DynamicContentController {
+
+  @Get('/dynamicContent/:key')
+  async getDynamicContent(@Param('key') key: string) {
+    const dynamicContent = await DynamicContent.findByPrimary<DynamicContent<any>>(key);
+
+    if (dynamicContent) return dynamicContent.toJSON();
+
+    throw new NotFoundError();
+  }
+
+}
