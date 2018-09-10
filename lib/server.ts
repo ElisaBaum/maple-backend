e36fe2e1da3840b6e2f0666731d91272131fb2ed
RobinBuschmann <robinbuschmann@hotmail.de>
2018-09-10 22:01:26 +0200

    Improves logging; Updates maple-ui

--- lib/server.ts
+++ lib/server.ts
@@ -2,8 +2,9 @@ import {injector} from "./injector";
 import {App} from "./app";
 
 const app = injector.get(App).getExpressApp();
+const port = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(port, () => {
   // tslint:disable:no-console
-  console.log('Server listening on port 3000!');
+  console.log(`Server listening on port ${port}!`);
 });
